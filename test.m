44ff1d86b9d7f634ace2e6e89d0b6a0bda10f577
clashley1400 <lashley.49@buckeyemail.osu.edu>
2025-10-10 08:37:31 -0400

    Update test.m

--- test.m
+++ test.m
@@ -17,7 +17,8 @@ for i = 1:Maxitr
 
   %Controller block here
   ui = f(err);
-  
+
+  %update step using integrator
   [t,Xi] = ode45(@Sat_template,[0,delt],[Xi,ui];
   plot3(Xi(:,1),Xi(:,2),Xi(:,3),'o');
 
