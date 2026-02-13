b4f73d68f19a27d81782dd9bcf0b6b3a32024466
clashley1400 <lashley.49@buckeyemail.osu.edu>
2026-02-13 12:11:51 -0500

    sputnik test

---
+++ src/Sputnik_test2.m
@@ -0,0 +1,32 @@
+%Testing the sat template
+Xi = [0, 0, 6678e3, -7789, 0, 0, 0.5709415, 0.1675188, 0.5709415, 0.5656758,0,0,0]';
+[t,y] = ode45(@Sat_template2_linear,[0 1000],Xi);
+
+%quaternions to euler angles
+q0 = y(:,7);
+q1 = y(:,8);
+q2 = y(:,9);
+q3 = y(:,10);
+wx = y(:,11);
+wy = y(:,12);
+wz = y(:,13);
+%coversion to roll pitch yaw
+
+
+%plotting
+%orientation euler angles
+figure(1)
+phi = atan2(2*(q0.*q1 + q2.*q3),1-2*(q1.^2+q2.^2));
+theta = asin(2*(q0.*q2-q1.*q3));
+psi = atan2(2*(q0.*q3 + q1.*q2),1-2*(q2.^2 + q3.^2));
+plot(t,phi,t,theta,t,psi);
+legend('Phi','Theta','Psi')
+
+%angular velocities
+figure(2)
+plot(t,wx,t,wy,t,wz);
+legend('omega x','omega y','omega z')
+
+%Position
+figure(3)
+plot3(y(:,1),y(:,2),y(:,3))
