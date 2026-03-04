ba14645ba28fb1ade9809c311e1f8f504439598e
clashley1400 <lashley.49@buckeyemail.osu.edu>
2026-03-04 15:23:12 -0500

    stuff

--- src/Reference_Trajectory_Calculation.m
+++ src/Reference_Trajectory_Calculation.m
@@ -1,8 +1,10 @@
-clc;clear;
+clc;clear; close all;
 
 t = 0:1:3600*.25;
 %initial location of reference point on the ground
-Ref_point = [6378e3*cos(30/57.3) 6378e3*sin(30/57.3) 4000];
+long = 30;
+lat = 45;
+Ref_point = [6378e3*cos(long/57.3)*cos(lat/57.3) 6378e3*sin(long/57.3)*cos(lat/57.3) 6378e3*sin(lat/57.3)];
 
 %angular velocity of Earth
 om = 7.29e-5;
@@ -10,20 +12,39 @@ om = 7.29e-5;
 %solve for reference point as Earth rotates
     for i = 2:length(t)
         r_ECEF_ECI = [cos(om .*t(i)) -sin(om .*t(i)) 0 ; sin(om .*t(i)) cos(om.*t(i)) 0; 0 0 1];
-        Ref_point(end+1,:) = r_ECEF_ECI * [6378e3*cos(30/57.3) 6378e3*sin(30/57.3) 4000]';
+        Ref_point(end+1,:) = r_ECEF_ECI * [6378e3*cos(long/57.3)*cos(lat/57.3) 6378e3*sin(long/57.3)*cos(lat/57.3) 6378e3*sin(lat/57.3)]';
     end
 
 %initial conditions of satellite
-Xi = [6378e3+400e3; 0; 0; 0; 7.672e3; 0; 0.5; -0.5; -0.5; 0.5; 0; 0; 0]';
+earthRadius = 6378.14e3; % Earth equatorial radius [m]
+a = 250e3 + earthRadius; % 250 km above Earth semimajor axis
+e = 0; % Eccentricity
+i = 0; % Inclination
+raan = 0; % Right ascension of ascending node
+aop = 0; % Argument of periapse
+ta = 0; % True anomaly
+
+orbit = [a, e, deg2rad(i), deg2rad(raan), deg2rad(aop), deg2rad(ta)];
+
+RV = RVfromOE(orbit);
+
+r_i = RV(:, 1)'; % [m]
+v_i = RV(:, 2)'; % [m/s]
+Xi = [r_i(1);r_i(2);r_i(3);v_i(1);v_i(2);v_i(3); 0.5; -0.5; -0.5; 0.5; 0; 0; 0]';
 
 %integrate to find satellite position
 [t, X] = ode45(@Sat_template, t, Xi);
 
 %plotting
 figure(1)
-hold on;
 plot3(X(:,1),X(:,2),X(:,3),'o');
+hold on;
 plot3(Ref_point(:,1),Ref_point(:,2),Ref_point(:,3),'*');
+E = wgs84Ellipsoid;
+[x,y,z] = ellipsoid(0, 0, 0, E.SemimajorAxis, E.SemimajorAxis, E.SemiminorAxis);
+surf(x, y, z, FaceAlpha="texturemap", FaceColor="texturemap", EdgeAlpha="texturemap"); % Plot the Earth
+legend('Satellite','Reference Location','Earth')
+title('Location of Satellite and Reference Location With Rotating Earth')
 axis equal;
 hold off;
 
@@ -35,8 +56,9 @@ end
 
 %plot pointing vector
 figure(2)
-hold on
 plot3(v_point(:,1),v_point(:,2),v_point(:,3),'*');
+hold on;
+title('Pointing Vector From Satellite to Reference Point in ECI');
 plot3(0,0,0,'o')
 
 q = zeros(4,length(v_point));
@@ -52,5 +74,16 @@ for i = 1:length(v_point)
 end
 
 figure(3)
+ang = ang.*57.3;
 plot(t,ang(1,:),'o',t,ang(2,:),'o',t,ang(3,:),'o');
+legend('Roll','Pitch','Yaw');
+title('Euler Angles of Satellite When Pointing to Reference Location')
+
+figure(4)
+q= q.*57.3;
+plot(t,q(1,:),'o',t,q(2,:),'o',t,q(3,:),'o',t,q(4,:));
+legend('q0','q1','q2','q3');
+title('Euler Angles of Satellite When Pointing to Reference Location')
+
+
 
